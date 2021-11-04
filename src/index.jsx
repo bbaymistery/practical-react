c6a29fcc00849b7fc1d594114ae110eda4630a22
Elgun <elgun.ezmemmedov@mail.ru>
2021-11-04 21:22:26 +0400

    updated movie project

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import { App } from "./App";
+// import { AppProvider } from "./context";
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <AppProvider> */}
+    <App />
+    {/* </AppProvider> */}
+  </React.StrictMode>,
+  document.getElementById("root")
+);
